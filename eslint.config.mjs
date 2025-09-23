c9a16f4016c0806c36adaee584ab8ef803733770
David Alvarez Barraza <104799508+Deiviidsito@users.noreply.github.com>
2025-09-23 15:49:51 -0300

    fix: eslint errores de sintaxis en todo el codigo

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,4 @@
 // @ts-check
-//Esta es una herramienta de análisis de código.
 import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
@@ -29,7 +28,13 @@ export default tseslint.config(
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
-    },
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto'
+        }
+      ]
+    }
   },
 );
