c8f6dfeb729bc03facdb3497f4baed1668cbbb02
David Alvarez Barraza <104799508+Deiviidsito@users.noreply.github.com>
2025-09-24 12:34:00 -0300

    feat: cors habilitada para implementar en el front-end

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,15 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Configuración de CORS
+  app.enableCors({
+    origin: 'http://localhost:3000', // URL del frontend Next.js
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+  
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 4000);
 }
